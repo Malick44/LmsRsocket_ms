6f034633858eebe1490a804fd386e1a37af64230
Malick44 <100083361+Malick44@users.noreply.github.com>
2023-04-03 19:04:06 -0700

    gateway removed

--- RSocketLMS/XLMS-GATEWAY/src/main/java/org/xlms/app/controller/UserClient.java
+++
@@ -1,9 +0,0 @@
-package org.xlms.app.controller;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.rsocket.service.RSocketExchange;
-import reactor.core.publisher.Mono;
-import org.xlms.app.dto.UserDto;
-public interface UserClient {
-    @RSocketExchange("user.create")
-    public Mono<UserDto> createUser(Mono<UserDto> monoUserDto);
-}
